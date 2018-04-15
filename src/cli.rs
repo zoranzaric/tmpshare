726d873cadc165b9c7cc4218b73bf4cda646b4bd
Zoran Zaric <zz@zoranzaric.de>
2018-04-15 10:07:18 +0200

    Fix tmpshare help
    fixes #5

--- src/cli.rs
+++ src/cli.rs
@@ -3,14 +3,14 @@ use std::path::PathBuf;
 
 /// Arguments for the `tmpshare` tool.
 #[derive(StructOpt, Debug)]
-#[structopt(name = "tmpshare")]
+#[structopt(name = "tmpshare", about = "A tool to share files")]
 pub enum TmpShareOpt {
-    #[structopt(name = "add")]
+    #[structopt(name = "add", about = "Adds files to tmpshare")]
     Add {
         #[structopt(help = "The file to add", parse(from_os_str))]
         filename: PathBuf,
     },
-    #[structopt(name = "serve")]
+    #[structopt(name = "serve", about = "Serves file via HTTP")]
     Serve {
         #[structopt(long = "address", help = "Sets the address to bind the HTTP server to",
                     default_value = "127.0.0.1")]
@@ -19,9 +19,9 @@ pub enum TmpShareOpt {
                     default_value = "8080")]
         port: u16,
     },
-    #[structopt(name = "list")]
+    #[structopt(name = "list", about = "Lists files served by tmpshare")]
     List {},
-    #[structopt(name = "cleanup")]
+    #[structopt(name = "cleanup", about = "Purges old files")]
     Cleanup {
         #[structopt(help = "Maximum age of a file")]
         days: u16,
